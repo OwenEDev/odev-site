eec5b4bb40896c659608884662613ab43c5cb684
Owen Evans <owen0879@gmail.com>
2024-05-18 22:04:26 +0100

    made nav bar sticky, introduced skipping issues - fix in progress

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,21 +1,38 @@
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function NavBar() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  const onScroll = () => {
+    setIsSticky(window.scrollY > 2);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  });
   return (
-    <div className={styles.glassNavBar}>
-      <nav>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <a href="#">Portfolio</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#">About</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div
+        className={`${styles.placeHolder} ${isSticky ? "visible" : ""}`}
+      ></div>
+      <div className={`${styles.glassNavBar} ${isSticky ? styles.sticky : ""}`}>
+        <nav>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <a href="#">Portfolio</a>
+            </li>
+            <li className={styles.navItem}>
+              <a href="#">About</a>
+            </li>
+            <li className={styles.navItem}>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
+"use client";
 import styles from "./page.module.css";
 import Background from "./background";
 import NavBar from "./navbar";
+import { useEffect } from "react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Background />
       <NavBar />
-      <div className="content"></div>
+      <div className="content">
+        <div className={styles.glassPage}></div>
+        <div className={styles.glassPage}></div>
+        <div className={styles.glassPage}></div>
+      </div>
     </main>
   );
 }
