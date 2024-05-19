5660a18148efadb6d707a56d7f62174d3f399c12
Owen Evans <owen0879@gmail.com>
2024-05-19 17:12:05 +0100

    scrolling logic added

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,38 +1,35 @@
-import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import { scroller } from "react-scroll";
 
 export default function NavBar() {
-  const [isSticky, setIsSticky] = useState(false);
-
-  const onScroll = () => {
-    setIsSticky(window.scrollY > 2);
+  const scrollToPage = (page) => {
+    scroller.scrollTo(page, {
+      duration: 800,
+      smooth: "easeInOutQuart",
+      offset: -30,
+    });
   };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  });
   return (
-    <>
-      <div
-        className={`${styles.placeHolder} ${isSticky ? "visible" : ""}`}
-      ></div>
-      <div className={`${styles.glassNavBar} ${isSticky ? styles.sticky : ""}`}>
-        <nav>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <a href="#">Portfolio</a>
-            </li>
-            <li className={styles.navItem}>
-              <a href="#">About</a>
-            </li>
-            <li className={styles.navItem}>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <div className={`${styles.glassNavBar}`}>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a href="#" onClick={() => scrollToPage("portfolio")}>
+              Portfolio
+            </a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" onClick={() => scrollToPage("about")}>
+              About
+            </a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" onClick={() => scrollToPage("contact")}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,18 @@
 import styles from "./page.module.css";
 import Background from "./background";
 import NavBar from "./navbar";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Background />
       <NavBar />
+      <div className={styles.navSpace} />
       <div className="content">
-        <div className={styles.glassPage}></div>
-        <div className={styles.glassPage}></div>
-        <div className={styles.glassPage}></div>
+        <div className={styles.glassPage} name="portfolio"></div>
+        <div className={styles.glassPage} name="about"></div>
+        <div className={styles.glassPage} name="contact"></div>
       </div>
     </main>
   );
